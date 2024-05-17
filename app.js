e246fdec2190709a1c1e7d2393de833dda85cd66
fashola tolulope <152597180+fasholatolulope3@users.noreply.github.com>
2024-05-17 20:23:30 +0100

    Add files via upload

---
+++ app.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    const menu = document.querySelector('.link-menu');
+    const icon = document.querySelector('.ham-icon');
+    menu.classList.toggle('active');
+    icon.classList.toggle('active');
+
+}
